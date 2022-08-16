ee1c7777a6def2dae51217630148e0794b159ca6
thiago carneiro <thiagocarneiro1998@gmail.com>
2022-08-16 14:55:43 -0300

    criada instancia de objetos command line runner

--- src/main/java/com/thiago/bookstore/BookstoreApplication.java
+++ src/main/java/com/thiago/bookstore/BookstoreApplication.java
@@ -1,13 +1,41 @@
 package com.thiago.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.thiago.bookstore.domain.Categoria;
+import com.thiago.bookstore.domain.Livro;
+import com.thiago.bookstore.repositories.CategoriaRepository;
+import com.thiago.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository caretegoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum",cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.caretegoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
+	
+	
+	
+	
 }
