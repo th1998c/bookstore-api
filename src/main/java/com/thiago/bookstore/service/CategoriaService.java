19afa80d90082e42ea7b8f0d190d3385da1f325b
thiago carneiro <thiagocarneiro1998@gmail.com>
2022-08-27 23:53:57 -0300

    tratada excessão DataIntegrityViolationException em delete

--- src/main/java/com/thiago/bookstore/service/CategoriaService.java
+++ src/main/java/com/thiago/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.thiago.bookstore.domain.Categoria;
@@ -41,7 +42,12 @@ public class CategoriaService {
 	
 	public void delete(Integer id) {
 		findById(id);
+		try {
 		repository.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new com.thiago.bookstore.service.exceptions.DataIntegrityViolationException(""
+					+ "Categoria não pode ser deletado pois possui livros associados.");
+		}
 	}
 
 	
